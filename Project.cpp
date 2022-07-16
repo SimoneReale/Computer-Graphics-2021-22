0df489d0f65198a2b2bd5ce748a444f5132a4f69
Simone Reale <srsimonereale98@hotmail.it>
2022-07-16 22:08:36 +0200

    cambio del nome dell'applicazione

--- Project.cpp
+++ Project.cpp
@@ -1169,7 +1169,7 @@ private:
     void createInstance() {
     	VkApplicationInfo appInfo{};
        	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    	appInfo.pApplicationName = "Hello Triangle";
+    	appInfo.pApplicationName = "Project CG REALE-STAGNOLI";
     	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     	appInfo.pEngineName = "No Engine";
     	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
